e354bef911ec5bf41d89e50e6c5cfc4d7bf98796
Zach Barr <zachbarr@zachbarr.com>
2022-08-24 19:26:59 -0400

    added capital allocation instead of size

--- first.py
+++ first.py
@@ -49,27 +49,29 @@ class TradeManager:
         )
         return bars.df
 
-def test_momentum(symbols, start, end, greens, candle_minutes, size):
+def test_momentum(symbols, start, end, greens, candle_minutes, capital):
     tm = TradeManager(
         BASE_URL,
         KEY_ID,
         SECRET_KEY
     )
     tm.get_account_status()
-    total_cap = 0
+    # total_cap = 0
     total_profit = 0
+    total_trades = 0
     for symbol in symbols:
         bars = tm.get_bars(symbol, start, end, candle_minutes)
         print(f"Sybol: {symbol}")
         profit = 0
         green = 0
         num_trades = 0
-        max_capital_allocated = 0
+        # max_capital_allocated = 0
         for index, bar in bars.iterrows():
             # print(index)
             # print(bar.symbol)
             if green == greens:
-                max_capital_allocated = max(max_capital_allocated, bar.open * size)
+                # max_capital_allocated = max(max_capital_allocated, bar.open * size)
+                size = math.floor(capital / bar.open)
                 diff = (bar.open - bar.close) * size
                 profit += diff
                 green = 0
@@ -84,23 +86,31 @@ def test_momentum(symbols, start, end, greens, candle_minutes, size):
                     green = 0
         print(f"Total profit: {profit}")
         print(f"Num trades: {num_trades}")
-        print(f"Max capital allocated: {max_capital_allocated}")
-        total_cap += max_capital_allocated
+        # print(f"Max capital allocated: {max_capital_allocated}")
+        # total_cap += max_capital_allocated
         total_profit += profit
-    print(f"\nTotal capital allocated: {total_cap}")
+        total_trades += num_trades
+    # print(f"\nTotal capital allocated: {total_cap}")
     print(f"Total profit: {total_profit}")
+    print(f"Total trades: {total_trades}")
 
 if __name__ == '__main__':
     test_momentum(
         [
-            'NFLX',
+            # 'NFLX',
+            'AMD',
             'AAPL',
-            'JPM',
-            'TSLA'
+            # 'JPM',
+            # 'TSLA'
+            'NVDA',
+            'OXY',
+            'F'
         ],
+        # '2022-05-02',
+        # '2022-08-19',
+        '2022-02-14',
         '2022-05-02',
-        '2022-08-19',
         5,
-        45,
-        20
+        3,
+        5000
     )
